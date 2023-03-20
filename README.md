26cd1848ad82493aa28397e36698024b1674f629
Joowww <72143824+Joowww@users.noreply.github.com>
2023-03-20 10:33:33 +0100

    version concurrente

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 #Mi Ejercicio Guia
 
+Version con conexion desconexion
+
+Version con concurrencia
 
 
--- servidorEjercicioGuia.c
+++ servidorEjercicioGuia.c
@@ -5,6 +5,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+/////
+///////
+/////////
 
 int main(int argc, char *argv[])
 {
